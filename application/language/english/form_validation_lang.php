28a3334f97758fedb7e2ad2f801d866cd22f8186
Tien <nguyenquoctien2000@gmail.com>
2022-06-07 23:55:33 +0700

    create push code github

---
+++ application/language/english/form_validation_lang.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2016, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_required']		= '{field} không được để trống !';
+$lang['form_validation_isset']			= 'The {field} field must have a value.';
+$lang['form_validation_valid_email']		= '{field} không đúng định dạng';
+$lang['form_validation_min_length']		= '{field} dài tối thiểu {param} kí tự';
+$lang['form_validation_max_length']		= '{field} tối đa chỉ được {param} kí tự.';
+$lang['form_validation_valid_emails']		= 'Email đã được sử dụng, vui lòng nhập email khác';
+$lang['form_validation_matches']		= '{field} không khớp.';
+$lang['form_validation_is_unique'] 		= '{field} đã là thành viên, vui lòng đăng nhập hoặc nhập {field} khác !';
+$lang['form_validation_regex_match']		= ' {field} không đúng định dạng.';
+$lang['form_validation_numeric']		= '{field} không đúng định dạng số.';
+
+
---
+++ application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,32 @@
+<!-- <h1>Trang giới thiệu</h1> -->
+<style>
+*{
+    color: black;
+}
+section {
+  color: #fff;
+  font-family: arial;
+  width: 500px;
+  margin: auto;
+  line-height: 20px;
+  font-size: 16px;
+}
+</style>
+
+    <section>
+        <h1 style="color: black">Giới thiệu</h1>
+            <p>We are a fairly small, flexible design studio that designs for print and web. We work flexibly with clients to fulfil their design needs. Whether you need to create a brand from scratch, including marketing materials and a beautiful and functional website or whether you are looking for a design refresh we are confident you will be pleased with the results.</p>
+            <p>We offer the following services:</p>
+        <ul>
+            <li>Branding</li>
+            <li>Logos</li>
+            <li>Websites</li>
+            <li>Web applications</li>
+        </ul>
+        <h1 style="color:black">Sub page</h1>
+            <p>Before you choose us to take on your project you will probably want to know a bit more about us, so meet the team:</p>
+                <!-- <img src="#"/> -->
+            <p>Ross has over 10 years experience in the industry. He is our Creative Director, digital designer, web designer and front-end developer. He is also pretty good with a sketchbook. Before starting the company Ross worked as a designer and studio manager for a design house who boasted a number of big name clients. Ross has brought his vast experience from this role to the work he does now.</p>
+            <p>Monica is Ross’ sister, our Art Director and specialises in graphic and print design. She has also worked with some big names and her designs have won her a number of industry awards.</p>
+                <!-- <img src="#"/> -->
+    </section>
